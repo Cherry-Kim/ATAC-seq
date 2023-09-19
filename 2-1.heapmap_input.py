8b7e8fd3d14b264548772d35b17ead7d177f55ff
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-09-19 13:37:07 -0700

    Update 2-1.heapmap_input.py

--- 2-1.heapmap_input.py
+++ 2-1.heapmap_input.py
@@ -2,17 +2,14 @@ import string,sys,os
   
 def step1_peaks():
     #VEH_OSI
-    os.system('bedtools intersect -a 975-VEH_S1_peaks.narrowPeak -b 975-OSI_S7_peaks.narrowPeak -v > VEH_OSI.specific.out')
-    os.system('bedtools intersect -a 975-OSI_S7_peaks.narrowPeak -b 975-VEH_S1_peaks.narrowPeak -v > OSI_VEH.specific.out')
-    os.system('bedtools intersect -a 975-VEH_S1_peaks.narrowPeak -b 975-OSI_S7_peaks.narrowPeak -wa > VEH_OSI.overlap.out')
+    os.system('bedtools intersect -a VEH_S1_peaks.narrowPeak -b OSI_S7_peaks.narrowPeak -v > VEH_OSI.specific.out')
+    os.system('bedtools intersect -a OSI_S7_peaks.narrowPeak -b VEH_S1_peaks.narrowPeak -v > OSI_VEH.specific.out')
+    os.system('bedtools intersect -a VEH_S1_peaks.narrowPeak -b OSI_S7_peaks.narrowPeak -wa > VEH_OSI.overlap.out')
 
 def step2_summits():
-    os.system('bedtools intersect -a 975-VEH_S1_summits.bed -b VEH_OSI.specific.out -wa > VEH_OSI.specific2.out')
-    #chr1    10150   10151   827-VEH_S1_peak_1       41.3352
-    #chr1    181391  181632  827-VEH_S1_peak_14      70      .       3.36226 8.99646 7.02083 83
-    #chr1    181474  181475  827-VEH_S1_peak_14      7.02083
-    os.system('bedtools intersect -a 975-VEH_S1_summits.bed -b VEH_OSI.overlap.out -wa > VEH_OSI.overlap2.out')
-    os.system('bedtools intersect -a 975-OSI_S7_summits.bed -b OSI_VEH.specific.out -wa > OSI_VEH.specific2.out')
+    os.system('bedtools intersect -a VEH_S1_summits.bed -b OSI.specific.out -wa > VEH_OSI.specific2.out')
+    os.system('bedtools intersect -a VEH_S1_summits.bed -b OSI.overlap.out -wa > VEH_OSI.overlap2.out')
+    os.system('bedtools intersect -a OSI_S7_summits.bed -b VEH.specific.out -wa > OSI_VEH.specific2.out')
 
 def step3_input():
   #ONT_specific
@@ -22,6 +19,18 @@ def step3_input():
   #VEH_specific
   chr1    10578   10579   975-VEH_S1_peak_1       6.1468
     os.system('cat OSI_VEH.specific2.out VEH_OSI.overlap2.out VEH_OSI.specific2.out > VEH_OSI.sum.bed')
-    os.system('cat ONT_VEH.specific2.out VEH_ONT.overlap2.out VEH_ONT.specific2.out > VEH_ONT.sum.bed')
-    os.system('cat TRC_VEH.specific2.out VEH_TRC.overlap2.out VEH_TRC.specific2.out > VEH_TRC.sum.bed')
 
+
+def step4_Heatmap():
+    #Deeptools
+    os.system('computeMatrix reference-point --referencePoint center -S VEH_S1_dedup.bw OSI_S7_dedup.bw -R VEH_OSI.sum.bed -b 2000 -a 2000 --skipZeros --numberOfProcessors 96 -o VEH_OSI_peaks.subtractCPM.gz --outFileSortedRegions VEH_OSI_peaks.subtractCPM.bed')
+    os.system('plotHeatmap --colorList white,darkred  --plotFileFormat pdf   --samplesLabel VEH ONT  --plotTitle "" -m VEH_OSI_peaks.subtractCPM.gz -out VEH_OSI_peaks.subtractCPM.heatmap.pdf --xAxisLabel "" ')
+
+def main():
+    #step1_peaks()
+    #step2_summits()
+    #step3_input()
+    step4_Heatmap()
+
+if __name__ == "__main__":
+    main()
